e613815d7110decdb5708b704b71a1bdff914c67
Austin <austin.berbereia@gmail.com>
2017-11-08 18:34:34 -0800

    Put basic output on the screen, with no styling.

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import _ from 'lodash';
 
 // Save the root URL for the API call and our API key.
 const ROOT_URL = 'https://api.openweathermap.org/data/2.5/weather?';
@@ -13,7 +14,7 @@ export default class App extends Component {
     super();
     this.state = {
       location: '',
-      icon: '',
+      iconURL: '',
       weather: '',
       temp: 0,
       tempUnit: 'C'
@@ -49,8 +50,8 @@ export default class App extends Component {
     const weatherData = axios.get(url).then(response => {
       this.setState({
         location: response.data.name,
-        icon: response.data.weather.icon,
-        weather: response.data.weather.main,
+        iconURL: "http://openweathermap.org/img/w/" + response.data.weather[0].icon + ".png",
+        weather: response.data.weather[0].main,
         temp: response.data.main.temp - 273.15,
         tempUnit: 'C'
       })
@@ -60,7 +61,24 @@ export default class App extends Component {
   // Finally, call the render function to get everything on screen.
   render() {
     return (
-      <div>{this.state.location}</div>
+      <div>
+        <div className="row">
+          <div className="col">What's the weather like in</div>
+        </div>
+        <div className="row">
+          <div className="col">{this.state.location}</div>
+        </div>
+        <div className="row">
+          <div className="col"><img src={this.state.iconURL}/></div>
+        </div>
+        <div className="row">
+          <div className="col">{this.state.weather}</div>
+        </div>
+        <div className="row">
+          <div className="col">{_.round(this.state.temp,2)}&#176;{this.state.tempUnit}</div>
+        </div>
+
+      </div>
     );
   }
 }
